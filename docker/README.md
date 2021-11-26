fb3e5c5e9e892af63f727f7c299cdcbe5497aed4
Eric Wang <wangchaogo1990@gmail.com>
2021-11-26 23:49:53 +0800

    add docker config

---
+++ docker/README.md
@@ -0,0 +1,5 @@
+- [FZF window not appearing until keypress in Neovim in Docker when using shortcut](
+  https://github.com/junegunn/fzf.vim/issues/497)
+- [docker change Ctrl+p to something else?](
+  https://stackoverflow.com/questions/20828657/docker-change-ctrlp-to-something-else)
+
--- setup.sh
+++ setup.sh
@@ -9,12 +9,20 @@ ln -sf $dot/zsh/zshrc .zshrc
 ln -sf $dot/rgignore .rgignore
 ln -si $dot/gitconfig .gitconfig
 
+p=".config/nvim"
+[[ ! -d $p ]] && mkdir -p $p
 if [ ! -d .config/nvim ]; then
-  mkdir -p .config/nvim
 fi
-cd .config/nvim
+cd $p
 ln -sf $dot/vim/init.vim init.vim
 ln -sf $dot/vim/coc-settings.json coc-settings.json
+cd -
+
+p=".docker"
+[[ ! -d $p ]] && mkdir $p
+cd $p
+ln -sf $dot/docker/config.json config.json
+cd -
 
 cd $current
 
