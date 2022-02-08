b48da763dfae8b887478d865f844ad4e9a5774a5
Eric Wang <wangchaogo1990@gmail.com>
2022-02-08 10:27:58 +0800

    add soft

---
+++ soft.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# nvim
+brew install node neovim ripgrep
+
+# lsd
+## [Icons render on MacOS](https://github.com/Peltoche/lsd/issues/199#issuecomment-494218334)
+brew install lsd && brew tap homebrew/cask-fonts && brew install font-hack-nerd-font
+
