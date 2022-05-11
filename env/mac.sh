5915bc837db8b9ab5f8f59e47ecfc95025da649f
Eric Wang <wangchaogo1990@gmail.com>
2022-05-11 16:19:59 +0800

    add rzsz

--- env/mac.sh
+++ env/mac.sh
@@ -22,7 +22,7 @@ pwpolicy -clearaccountpolicies
 brew install node neovim ripgrep
 python3 -m pip install --user --upgrade pynvim
 
-brew install tree
+brew install tree lrzsz
 
 # lsd
 ## [Icons render on MacOS](https://github.com/Peltoche/lsd/issues/199#issuecomment-494218334)
