ed762fd198480c5f9f1de25c91cc788392fbe00a
Eric Wang <wangchaogo1990@gmail.com>
2024-03-30 11:08:25 +0800

    use dashboard for lazyvim

--- lazyvim/lua/plugins/plugin.lua
+++ lazyvim/lua/plugins/plugin.lua
@@ -1,4 +1,39 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup({
+        -- config
+        theme = "hyper",
+        config = {
+          week_header = {
+            enable = true,
+            concat = "Je pense donc je suis",
+          },
+          shortcut = {
+            { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+            {
+              icon = " ",
+              icon_hl = "@variable",
+              desc = "Files",
+              group = "Label",
+              action = "Telescope find_files",
+              key = "f",
+            },
+            {
+              desc = " LiveGrep",
+              group = "Files",
+              action = "Telescope live_grep",
+              key = "g",
+            },
+          },
+        },
+      })
+    end,
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  },
 }
