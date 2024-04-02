7f1092ab8c77b8790a26555d8279423c53d1dc5f
Eric Wang <wangchaogo1990@gmail.com>
2024-04-02 19:56:02 +0800

    install packer automatically

--- vim/packer/lua/plugins/plugins.lua
+++ vim/packer/lua/plugins/plugins.lua
@@ -1,4 +1,16 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- automatically install if not exist
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -66,5 +78,11 @@ return require('packer').startup(function(use)
   use 'ianva/vim-youdao-translater'
 
   use 'solarnz/thrift.vim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
