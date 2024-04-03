be3eb37a96df50062db01df1cf259b4d66e07d6d
Eric Wang <wangchaogo1990@gmail.com>
2024-04-03 09:27:33 +0800

    put coc in which key

--- vim/packer/lua/keymaps.lua
+++ vim/packer/lua/keymaps.lua
@@ -1,11 +1,11 @@
 -- telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<c-e>', builtin.buffers, {})
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<c-p>', telescope.find_files, {})
+vim.keymap.set('n', '<c-e>', telescope.buffers, {})
 
 -- youdao translator
-vim.keymap.set('n', '<c-t>', ':<C-u>Ydc<CR>', {silent = true})
-vim.keymap.set('v', '<c-t>', ':<C-u>Ydv<CR>', {silent = true, noremap = true})
+vim.keymap.set('n', '<c-t>', ':<C-u>Ydc<CR>', { silent = true })
+vim.keymap.set('v', '<c-t>', ':<C-u>Ydv<CR>', { silent = true, noremap = true })
 vim.keymap.set('n', '<leader>yd', ':<C-u>Yde<CR>', {})
 
 -- which key
@@ -13,26 +13,36 @@ local wk = require('which-key')
 wk.register({
   f = {
     name = "File",
-    g = {"<cmd>Telescope live_grep<cr>", "Live Grep"},
-    t = {"<cmd>terminal<cr>i", "Terminal"},
-    f = {"<cmd>Telescope find_files<cr>", "Find File"},
-    h = {"<cmd>Telescope help_tags<cr>", "Help Tags"}
+    g = { "<cmd>Telescope live_grep<cr>", "live Grep" },
+    t = { "<cmd>terminal<cr>i", "Terminal" },
+    f = { "<cmd>Telescope find_files<cr>", "find File" },
+    h = { "<cmd>Telescope help_tags<cr>", "Help tags" }
   },
+  t = { "<cmd>terminal<cr>i", "Terminal" },
   s = {
     name = "Search",
-    o = {"<cmd>Telescope vim_options<cr>", "Options"},
-    k = {"<cmd>Telescope keymaps<cr>", "Key Maps"},
+    o = { "<cmd>Telescope vim_options<cr>", "Options" },
+    k = { "<cmd>Telescope keymaps<cr>", "Key Maps" },
   },
   g = {
     name = "Git",
-    b = {"<cmd>Git blame<cr>", "Git blame"},
-    g = {"<cmd>LazyGit<cr>", "LazyGit"},
-    c = {"<cmd>Telescope git_commits<CR>", "Commits"},
-    s = {"<cmd>Telescope git_status<CR>", "Status"}
+    b = { "<cmd>Git blame<cr>", "git Blame" },
+    g = { "<cmd>LazyGit<cr>", "lazyGit" },
+    c = { "<cmd>Telescope git_commits<CR>", "Commits" },
+    s = { "<cmd>Telescope git_status<CR>", "Status" }
   },
   c = {
     name = "code",
-    r = {"<Plug>(coc-rename)", "Rename"}
-  }
-}, {prefix = "<leader>"})
-
+    r = { "<Plug>(coc-rename)", "Rename" },
+    f = { "<cmd>Format <cr>", "Format" },
+    i = { "<cmd>OR <cr>", "organize Imports" },
+    d = { "<cmd>CocList diagnostics<cr>", "show all Diagnostics" },
+    o = { "<cmd>CocList outline<cr>", "Outline to find symbols of current document" },
+    a = { "<Plug>(coc-codeaction-source)", "code Actions for current file" },
+    c = {
+      name = "Coc",
+      e = { "<cmd>CocList extensions<cr>", "CoC extensions" }
+    }
+  },
+  -- y = { "<cmd><c-u>Yde<cr>", "Youdao translator" }
+}, { prefix = "<leader>" })
--- vim/packer/lua/plugins/coc.lua
+++ vim/packer/lua/plugins/coc.lua
@@ -100,20 +100,20 @@ vim.api.nvim_create_autocmd("User", {
 -- Apply codeAction to the selected region
 -- Example: `<leader>aap` for current paragraph
 local opts = {silent = true, nowait = true}
-keyset("x", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
-keyset("n", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
+-- keyset("x", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
+-- keyset("n", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
 
 -- Remap keys for apply code actions at the cursor position.
-keyset("n", "<leader>ac", "<Plug>(coc-codeaction-cursor)", opts)
+-- keyset("n", "<leader>ac", "<Plug>(coc-codeaction-cursor)", opts)
 -- Remap keys for apply source code actions for current file.
-keyset("n", "<leader>as", "<Plug>(coc-codeaction-source)", opts)
+-- keyset("n", "<leader>as", "<Plug>(coc-codeaction-source)", opts)
 -- Apply the most preferred quickfix action on the current line.
-keyset("n", "<leader>qf", "<Plug>(coc-fix-current)", opts)
+-- keyset("n", "<leader>qf", "<Plug>(coc-fix-current)", opts)
 
 -- Remap keys for apply refactor code actions.
-keyset("n", "<leader>re", "<Plug>(coc-codeaction-refactor)", { silent = true })
-keyset("x", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
-keyset("n", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
+-- keyset("n", "<leader>re", "<Plug>(coc-codeaction-refactor)", { silent = true })
+-- keyset("x", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
+-- keyset("n", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
 
 -- Run the Code Lens actions on the current line
 keyset("n", "<leader>cl", "<Plug>(coc-codelens-action)", opts)
@@ -169,19 +169,19 @@ vim.opt.statusline:prepend("%{coc#status()}%{get(b:,'coc_current_function','')}"
 ---@diagnostic disable-next-line: redefined-local
 local opts = {silent = true, nowait = true}
 -- Show all diagnostics
-keyset("n", "<space>a", ":<C-u>CocList diagnostics<cr>", opts)
+-- keyset("n", "<space>a", ":<C-u>CocList diagnostics<cr>", opts)
 -- Manage extensions
-keyset("n", "<space>e", ":<C-u>CocList extensions<cr>", opts)
+-- keyset("n", "<space>e", ":<C-u>CocList extensions<cr>", opts)
 -- Show commands
 keyset("n", "<space>c", ":<C-u>CocList commands<cr>", opts)
 -- Find symbol of current document
-keyset("n", "<space>o", ":<C-u>CocList outline<cr>", opts)
+-- keyset("n", "<space>o", ":<C-u>CocList outline<cr>", opts)
 -- Search workspace symbols
 keyset("n", "<space>s", ":<C-u>CocList -I symbols<cr>", opts)
 -- Do default action for next item
-keyset("n", "<space>j", ":<C-u>CocNext<cr>", opts)
+-- keyset("n", "<space>j", ":<C-u>CocNext<cr>", opts)
 -- Do default action for previous item
-keyset("n", "<space>k", ":<C-u>CocPrev<cr>", opts)
+-- keyset("n", "<space>k", ":<C-u>CocPrev<cr>", opts)
 -- Resume latest coc list
-keyset("n", "<space>p", ":<C-u>CocListResume<cr>", opts)
+-- keyset("n", "<space>p", ":<C-u>CocListResume<cr>", opts)
 
