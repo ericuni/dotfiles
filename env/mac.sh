04da3116a2200571b7460b1441cb1e6a49b50eb1
Eric Wang <ericuni90+github@gmail.com>
2024-09-18 22:50:30 +0800

    mac env (#10)

--- env/mac.sh
+++ env/mac.sh
@@ -3,40 +3,44 @@ set -x
 
 source util.sh || exit 1
 
-# github already has brew installed
-if [[ -z $GITHUB_ACTIONS ]]; then
-  # brew
-  which brew
-  if [[ $? -eq 0 ]]; then
-    brew update  ## Update homebrew itself and the package lists
+action="update"
 
-    brew upgrade --formula
-
-    # original brew can only pin formula not cask
-    # [buo/homebrew-cask-upgrade](https://github.com/buo/homebrew-cask-upgrade) supports pinning cask
-    # brew upgrade --cask --greedy
-    for app in adrive docker qq postman v2rayu logseq obsidian raycast
-    do
-      brew cu pin $app
-    done
-    brew cu -a
-
-    brew cleanup
-
-    exit 0
-  fi
-
-  # install brew
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-  ## 去掉各种设置密码的规则限制
-  ## https://apple.stackexchange.com/questions/337468/how-to-set-a-short-user-password-in-macos-mojave-and-later-10-14
-  pwpolicy -clearaccountpolicies
-
-  ## 禁止.DS_Store生成
-  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
-
+# always run init in Github Actions
+if [[ -n $GITHUB_ACTIONS ]]; then
+  action="init"
 fi
+
+# brew
+which brew
+if [[ $? -ne 0 ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if [[ $action == "update" ]]; then
+  brew update  ## Update homebrew itself and the package lists
+
+  brew upgrade --formula
+
+  # original brew can only pin formula not cask
+  # [buo/homebrew-cask-upgrade](https://github.com/buo/homebrew-cask-upgrade) supports pinning cask
+  # brew upgrade --cask --greedy
+  for app in adrive docker qq postman v2rayu logseq obsidian raycast
+  do
+    brew cu pin $app
+  done
+  brew cu -a
+
+  brew cleanup
+
+  exit 0
+fi
+
+## 去掉各种设置密码的规则限制
+## https://apple.stackexchange.com/questions/337468/how-to-set-a-short-user-password-in-macos-mojave-and-later-10-14
+pwpolicy -clearaccountpolicies
+
+## 禁止.DS_Store生成
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 
 # suports `brew cu pin`
 brew tap buo/cask-upgrade
@@ -53,7 +57,7 @@ npm install -g neovim  ## make coc to work with gopls
 ## [Icons render on MacOS](https://github.com/Peltoche/lsd/issues/199#issuecomment-494218334)
 brew install lsd
 ## need change iterm font
-brew tap homebrew/cask-fonts && brew install font-hack-nerd-font
+brew install --cask font-hack-nerd-font
 
 brew install zoxide fzf starship direnv
 
