d4f4d7c05b39facb45d60872fc8064f2f5aeeaf8
ericuni <ericuni90+github@gmail.com>
2024-12-23 15:03:52 +0800

    add clipboard

--- env/mac.sh
+++ env/mac.sh
@@ -55,6 +55,10 @@ brew install node
 # python3 -m pip install --user --upgrade pynvim
 npm install -g neovim  ## make coc to work with gopls
 
+# https://github.com/sindresorhus/clipboard-cli
+# echo xxx | clipboard, 这样 xxx 就在剪贴板中了
+npm install --global clipboard-cli
+
 # lsd
 ## [Icons render on MacOS](https://github.com/Peltoche/lsd/issues/199#issuecomment-494218334)
 brew install lsd
