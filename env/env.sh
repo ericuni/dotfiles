c2b2bae1e7611b94fbb7c187e72eda27953f6491
ericuni <ericuni90+github@gmail.com>
2025-03-22 12:02:09 +0800

    rename

--- env/env.sh
+++ env/env.sh
@@ -1,17 +1,15 @@
-# export proxy_ip="127.0.0.1"
+# export proxy_loc="127.0.0.1:8118"
 
 function proxy() {
-  local loc="$proxy_ip:8118"
-  echo "with proxy $loc" >&2
-  http_proxy=$loc https_proxy=$loc no_proxy="$no_proxy" $@
+  echo "with proxy $proxy_loc" >&2
+  http_proxy=$proxy_loc https_proxy=$proxy_loc no_proxy="$no_proxy" $@
 }
 
 function switch_proxy() {
   # -z: zero
   if [[ -z $http_proxy ]]; then
-    local loc="$proxy_ip:8118"
-    export http_proxy=$loc
-    export https_proxy=$loc
+    export http_proxy=$proxy_loc
+    export https_proxy=$proxy_loc
     echo "proxy $loc on" >&2
   else
     unset http_proxy
